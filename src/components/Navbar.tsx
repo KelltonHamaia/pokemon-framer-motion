f7af2fb860d889b8c35aa3baa4d635228e13b39a
kellton hamaia <hamaia.kell@gmail.com>
2024-01-16 10:56:50 -0300

    fix:navbar is now responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,28 @@
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+
     return (
         <nav>
-            <ul className="flex items-center justify-end gap-5 divide-x-2">
-                <Link to="/">Home</Link>   
-                <Link to="/gymleaders">gymleaders</Link>
-                <Link to="/eliteFour">eliteFour</Link>   
+            <ul className="flex items-center justify-end gap-4 mr-4 divide-x-2">
+                <Link
+                    className='pl-3 text-sm font-semibold text-center md:text-regular' 
+                    to="/"
+                >
+                    Home
+                </Link>   
+                <Link
+                    className='pl-3 text-sm font-semibold text-center md:text-regular' 
+                    to="/gymleaders"
+                >
+                    Gym Leaders
+                </Link>
+                <Link
+                    className='pl-3 text-sm font-semibold text-center md:text-regular' 
+                    to="/eliteFour"
+                >
+                    Elite Four
+                </Link>   
             </ul>
         </nav>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,27 +43,27 @@ export const Home = () => {
             <Navbar />
 
             <h1 className="py-4 text-2xl font-bold tracking-tighter text-center md:py-5">Pokémon Crystal</h1>
-            <motion.img src="/images/pokemon.jpg" alt="pokemon" className="w-full p-4 mx-auto rounded md:h-96"
+            <motion.img src="/images/pokemon.jpg" alt="pokemon" className="w-full px-4 mx-auto rounded-lg md:h-96"
                 variants={childrenVariants}
             />
 
             <motion.section
                 variants={childrenVariants}
             >
-                <h2 className="py-5 ml-2 text-2xl font-semibold tracking-tighter">História</h2>
-                <p className="p-4">
+                <h2 className="py-4 ml-4 text-2xl font-semibold tracking-tighter md:py-5">História</h2>
+                <p className="px-4 py-1 md:p-4">
                     O jogo se passa na região de Johto, e protagoniza a segunda geração de jogos Pokémon,
                     onde dessa vez, estamos na pele de Ethan ou da novs personagem Kris. Você recebe seu pokémon do Prof. Elm, encara ginásios,
                     e conforme avança no jogo, descobre que a Equipe Rocket voltou a funcionar e o seu Líder, Giovanni, também está de volta. Então você os combate,
-                    e por fim encara a Elite Four, composta por: Will, Koga, Bruno, Karen e por fim, Lance!
+                    e por fim, desafia a Elite Four, composta por: Will, Koga, Bruno, Karen e por fim, Lance!
                 </p>
             </motion.section>
 
             <motion.section
                 variants={childrenVariants}
             >
-                <h2 className="py-5 ml-2 text-2xl font-semibold tracking-tighter">Característias</h2>
-                <p className="p-4">
+                <h2 className="py-4 ml-4 text-2xl font-semibold tracking-tighter md:py-5 text-pretty">Característias</h2>
+                <p className="px-4 py-1 md:p-4">
                     Algumas características dessa série, é que
                     é a primeira que possui o sistema de Day-care, isso é, deixar um pokemon neste lugar, para que ele evolua, além da possibilidade de
                     <em> breeding</em>, ou seja, se o pokémon tiver um outro pokemon junto e que seja compatível, existe uma chance de você receber um ovo de um dos NPC's!
